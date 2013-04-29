74f0be2c586169b489dfad5d7e1f4bdddb3dccdb
Neo Lee <neo.lee@gmail.com>
2013-04-29 13:57:56 +0800

    Site updated at 2013-04-29 05:57:56 UTC

---
+++ javascripts/footnote.js
@@ -0,0 +1,78 @@
+jQuery.noConflict();
+
+jQuery(document).ready(function() {
+  Footnotes.setup();
+});
+
+var Footnotes = {
+  footnotetimeout: false,
+  setup: function() {
+    var footnotelinks = jQuery("a[rel='footnote']")
+        
+    footnotelinks.unbind('mouseover',Footnotes.footnoteover);
+    footnotelinks.unbind('mouseout',Footnotes.footnoteoout);
+        
+    footnotelinks.bind('mouseover',Footnotes.footnoteover);
+    footnotelinks.bind('mouseout',Footnotes.footnoteoout);
+  },
+  footnoteover: function() {
+    clearTimeout(Footnotes.footnotetimeout);
+    jQuery('#footnotediv').stop();
+    jQuery('#footnotediv').remove();
+        
+    var id = jQuery(this).attr('href').substr(1);
+    var position = jQuery(this).offset();
+    
+    var div = jQuery(document.createElement('div'));
+    div.attr('id','footnotediv');
+    div.bind('mouseover',Footnotes.divover);
+    div.bind('mouseout',Footnotes.footnoteoout);
+
+    var el = document.getElementById(id);
+    div.html(jQuery(el).html());
+    div.html($(el).html());
+    div.find("a[rev='footnote']").remove();
+        
+    div.css({
+      position: 'absolute',
+      width: '400px',
+      padding: '5px 10px 0px 10px',
+      border: 'solid 1px',
+      background: '#f6f6c3',
+      'font-size': '80%',
+      'line-height': 1.4,
+      '-moz-border-radius': '.5em',
+      '-webkit-border-radius': '.5em',
+      'border-radius': '.5em',
+      opacity: '0.95'
+    });
+    jQuery(document.body).append(div);
+
+    var left = position.left;
+    if(left + 420  > jQuery(window).width() + jQuery(window).scrollLeft())
+    left = jQuery(window).width() - 420 + jQuery(window).scrollLeft();
+    var top = position.top + 20;
+    if(top + div.height() > jQuery(window).height() + jQuery(window).scrollTop())
+    top = position.top - div.height() - 15;
+    div.css({
+      left: left,
+      top: top
+    });
+  },
+  footnoteoout: function() {
+    Footnotes.footnotetimeout = setTimeout(function() {
+      jQuery('#footnotediv').animate({
+        opacity: 0
+      }, 600, function() {
+        jQuery('#footnotediv').remove();
+      });
+    },100);
+  },
+  divover: function() {
+    clearTimeout(Footnotes.footnotetimeout);
+    jQuery('#footnotediv').stop();
+    jQuery('#footnotediv').css({
+      opacity: 0.9
+    });
+  }
+}
