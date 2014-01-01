f11c8df4730fc3d9218e2ac7879e2e7b816ea5e4
Neo Lee <neo.lee@gmail.com>
2014-01-02 00:30:40 +0800

    Site updated at 2014-01-01 16:30:40 UTC

--- javascripts/footnote.js
+++ javascripts/footnote.js
@@ -9,11 +9,11 @@ var Footnotes = {
   setup: function() {
     var footnotelinks = jQuery("a[rel='footnote']")
         
-    footnotelinks.unbind('mouseover',Footnotes.footnoteover);
-    footnotelinks.unbind('mouseout',Footnotes.footnoteoout);
+    footnotelinks.unbind('mouseover', Footnotes.footnoteover);
+    footnotelinks.unbind('mouseout', Footnotes.footnoteoout);
         
-    footnotelinks.bind('mouseover',Footnotes.footnoteover);
-    footnotelinks.bind('mouseout',Footnotes.footnoteoout);
+    footnotelinks.bind('mouseover', Footnotes.footnoteover);
+    footnotelinks.bind('mouseout', Footnotes.footnoteoout);
   },
   footnoteover: function() {
     clearTimeout(Footnotes.footnotetimeout);
@@ -24,9 +24,9 @@ var Footnotes = {
     var position = jQuery(this).offset();
     
     var div = jQuery(document.createElement('div'));
-    div.attr('id','footnotediv');
-    div.bind('mouseover',Footnotes.divover);
-    div.bind('mouseout',Footnotes.footnoteoout);
+    div.attr('id', 'footnotediv');
+    div.bind('mouseover', Footnotes.divover);
+    div.bind('mouseout', Footnotes.footnoteoout);
 
     var el = document.getElementById(id);
     div.html(jQuery(el).html());
@@ -35,28 +35,27 @@ var Footnotes = {
         
     div.css({
       position: 'absolute',
-      width: '400px',
-      padding: '5px 10px 0px 10px',
+      padding: '10px 10px 0px 10px',
       border: 'solid 1px',
-      background: '#f6f6c3',
-      'font-size': '80%',
+      background: '#e2e2e2',
       'line-height': 1.4,
+      'font-size': '90%',
       '-moz-border-radius': '.5em',
       '-webkit-border-radius': '.5em',
       'border-radius': '.5em',
-      opacity: '0.95'
+      opacity: '0.9'
     });
     jQuery(document.body).append(div);
 
     var left = position.left;
     if(left + 420  > jQuery(window).width() + jQuery(window).scrollLeft())
-    left = jQuery(window).width() - 420 + jQuery(window).scrollLeft();
+      left = jQuery(window).width() - 420 + jQuery(window).scrollLeft();
     var top = position.top + 20;
     if(top + div.height() > jQuery(window).height() + jQuery(window).scrollTop())
-    top = position.top - div.height() - 15;
+      top = position.top - div.height() - 15;
     div.css({
-      left: left,
-      top: top
+      left:left,
+      top:top
     });
   },
   footnoteoout: function() {
